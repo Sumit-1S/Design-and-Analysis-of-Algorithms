1a3a9e5ed3d43cd81a64d5afcda90214087a042f
Sumit Singh <sumitsingh130801@gmail.com>
2021-11-11 22:32:29 +0530

    Weekly Submission

---
+++ Week11_2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int coin(int *arr,int n,int t)
+{
+  if(t==0) return 1;
+  if(t<0) return 0;
+  if(n<=0 and t>=1) return 0;
+  return coin(arr,n-1,t) + coin(arr,n,t-arr[n-1]);
+}
+
+int main()
+{
+    int n,t;
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cin>>t;
+    cout<<coin(arr,n,t);
+    return 0;
+}
