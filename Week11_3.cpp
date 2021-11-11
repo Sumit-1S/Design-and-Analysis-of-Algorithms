4a4a0105a57fd7f85d2abafbf88600954adc088b
Sumit Singh <sumitsingh130801@gmail.com>
2021-11-11 22:42:08 +0530

    Weekly Submission

---
+++ Week11_3.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool SumSubset(int *arr,int n,int sum)
+{
+  if(sum==0)          return true;
+  if(n==0 and sum!=0) return false;
+  if(arr[n-1]>sum)    return SumSubset(arr,n-1,sum);
+  return SumSubset(arr,n-1,sum) or SumSubset(arr,n-1,sum-arr[n-1]);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr = new int[n];
+    int sum=0;
+    for(int i=0;i<n;i++)  cin>>arr[i];
+    for(int i=0;i<n;i++)  sum+=arr[i];
+    SumSubset(arr,n,sum/2)?cout<<"Yes":cout<<"No";
+    return 0;
+}
